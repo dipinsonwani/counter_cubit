f6a466d800c9a644bfffb30bd164806a4dbc6a29
dipinsonwani <dipinsonwani@gmail.com>
2021-03-13 15:30:28 +0530

    Add BlocProvider to provide bloc to the subtree

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:cubit_counter/cubit/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cubit/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +11,14 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
     );
   }
